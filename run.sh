99bbd10b6cad2001bd0f80c1b84fcce189f5a599
NaohiroTawara <tawara@pcl.cs.waseda.ac.jp>
2017-08-30 04:41:28 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+# training
 python train_autoencoder.py ../datasets/cam1_person001 --gpu 0 --batchsize 32 --epoch 100 --unit 100
 
+# evaluating
+python calc_score.py ../datasets/cam1_person002/  result/100.npz --unit 100
